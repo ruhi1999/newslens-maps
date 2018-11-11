560bded889a3c599a2a9ecfaedb4c74cc8ce0c73
Ruhi Pudipeddi <ruhi@berkeley.edu>
2018-11-11 09:52:06 -0800

    updated original script

--- folium1.py
+++ folium1.py
@@ -12,6 +12,7 @@ place_data = json.load(place_file)
 geolocator = Nominatim(user_agent = "NewsLens")
 
 mapper = folium.Map(location=[45.5236, -122.6750])
+topic = "2300"
 
 def color(count):
     if count > 100:
@@ -22,8 +23,12 @@ def color(count):
         return 'green'
 
 def lat_long(name):
-    loc = geolocator.geocode(name)
-    return [loc.latitude, loc.longitude]
+    loc = geolocator.geocode(name, timeout=2000)
+    if loc is not None:
+        coords = [loc.latitude, loc.longitude]
+        print(loc.address)
+        return coords
+    return "*****not found: "  + name
 
 fg = folium.FeatureGroup(name = "topic 23000")
 
@@ -31,15 +36,17 @@ places = {}
 lat = []
 long = []
 for item in place_data:
-    lat_long = lat_long(item['place_name'])
-    lat += lat_long[0]
-    long += lat_long[1]
+    coords = lat_long(item['place_name'])
+    if type(coords) is str:
+        print(coords)
+        continue
+    lat += [coords[0]]
+    long += [coords[1]]
     name = item['place_name']
     count = item['count']
     places.update({name: [count, lat_long]})
-    fg.add_child(folium.Marker(location = lat_long), popup = folium.Popup(name),
-        icon=folium.Icon(color=color(count),icon_color='green')
-
+    fg.add_child(folium.Marker(location = coords, popup = folium.Popup(name),
+        icon=folium.Icon(color=color(count),icon_color='green')))
 
 
 mapper = folium.Map(location=[mean(lat), mean(long)], zoom_start=6, tiles='Mapbox bright')
@@ -47,4 +54,4 @@ mapper = folium.Map(location=[mean(lat), mean(long)], zoom_start=6, tiles='Mapbo
 mapper.add_child(fg)
 
 mapper.add_child(folium.LayerControl())
-mapper.save(outfile='map.html')
+mapper.save(outfile='map' + topic + '.html')
